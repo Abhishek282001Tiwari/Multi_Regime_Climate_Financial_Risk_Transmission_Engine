32e72d8b5dea1b72c8e41636968f23fc431311f3
Abhishek Tiwari <136213318+Abhishek282001Tiwari@users.noreply.github.com>
2025-07-07 06:22:51 +0100

    Update __init__.py

--- src/data_ingestion/__init__.py
+++ src/data_ingestion/__init__.py
@@ -1,1 +1,8 @@
+"""
+Data Ingestion Module
+Handles collection and preprocessing of financial and climate data from FREE sources only.
+"""
 
+from .financial_data_collector import FinancialDataCollector
+
+__all__ = ['FinancialDataCollector']
