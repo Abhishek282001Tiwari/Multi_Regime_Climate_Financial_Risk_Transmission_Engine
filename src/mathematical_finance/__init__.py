1b8710935366da81e20268a2be02dc2aa385cb69
Abhishek Tiwari <136213318+Abhishek282001Tiwari@users.noreply.github.com>
2025-07-07 06:38:55 +0100

    Update __init__.py

--- src/mathematical_finance/__init__.py
+++ src/mathematical_finance/__init__.py
@@ -1,1 +1,8 @@
+"""
+Mathematical Finance Module
+Advanced stochastic models including jump-diffusion processes.
+"""
 
+from .jump_diffusion_model import JumpDiffusionModel
+
+__all__ = ['JumpDiffusionModel']
