697fcffb4a1165dbd4cef39e5ca4af102cdae4c0
Abhishek Tiwari <136213318+Abhishek282001Tiwari@users.noreply.github.com>
2025-07-07 06:36:23 +0100

    Update __init__.py

--- src/econometric_modeling/__init__.py
+++ src/econometric_modeling/__init__.py
@@ -1,1 +1,8 @@
+"""
+Econometric Modeling Module
+Advanced regime-switching and time series models for climate-financial analysis.
+"""
 
+from .markov_regime_switching import MarkovRegimeSwitching
+
+__all__ = ['MarkovRegimeSwitching']
